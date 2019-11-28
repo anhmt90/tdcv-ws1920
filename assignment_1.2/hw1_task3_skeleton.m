70daab52d426a9cd4bd07ee4e4a3a2c012633de2
Maria Avila Gonzalez <ge25qub@mytum.de>
2019-11-28 18:46:09 +0100

    Implemented points 1-4 from the script. Missing actual non-linear optimization (point 5)

--- assignment_1.2/hw1_task3_skeleton.m
+++ assignment_1.2/hw1_task3_skeleton.m
@@ -4,16 +4,26 @@ close all
 addpath('helper_functions')
 
 %% Setup
-% path to the images folder
-path_img_dir = 'data/tracking/validation/img';
+% % path to the images folder
+% path_img_dir = 'data/tracking/validation/img';
+% % path to object ply file
+% object_path = 'data/teabox.ply';
+% % path to results folder
+% results_path = 'data/tracking/validation/results';
+
+% There are two folders inside the data, I think we have to start with the
+% testing. 
+path_img_dir = 'data/tracking/test/img';
 % path to object ply file
 object_path = 'data/teabox.ply';
-% path to results folder
-results_path = 'data/tracking/validation/results';
+% % path to results folder
+results_path = 'data/tracking/test/results';
 
 % Read the object's geometry 
 % Here vertices correspond to object's corners and faces are triangles
 [vertices, faces] = read_ply(object_path);
+faces = faces + 1;
+
 
 % Create directory for results
 if ~exist(results_path,'dir') 
@@ -96,13 +106,14 @@ sift_scores=cell(1,1);
 
 % Default threshold for SIFT keypoints matching: 1.5
 % % When taking higher value, match is only recognized if similarity is very high
-threshold_ubcmatch = 2;
+threshold_ubcmatch = 2.5;
 % Match features between SIFT model and SIFT features from new image
 [sift_matches{1} , sift_scores{1}]= vl_ubcmatch(descriptors{1}, model.descriptors, threshold_ubcmatch);
 
 figure
 % Plot the image and the matched points on top.
 imshow (char(Filenames(1)));
+title('Matched points between model and 1st frame')
 hold on;
 % Visualize the matched points of the image
 vl_plotframe(keypoints{1}(:,sift_matches{1}(1,:)), 'linewidth',2);
@@ -116,11 +127,11 @@ best_inliers_set = cell(1);
 init_world_locations = zeros(1,3);
 init_world_orientations = zeros(3,3);
 
-ransac_iterations = 100; %input('Please select the number of iterations:','s');
-threshold_ransac = 4; %input('Please select the threshold for RANSAC method:','s');
+ransac_iterations = 300; %input('Please select the number of iterations:','s');
+threshold_ransac = 10; %input('Please select the threshold for RANSAC method:','s');
 %     TODO: Implement the RANSAC algorithm here
 
-[best_inliers_set, max_num_inliers] = RANSAC(ransac_iterations, threshold_ransac, sift_matches{1}, keypoints{1}, model.coord3d, camera_params);
+[best_inliers_set, max_num_inliers] = ransac_function(ransac_iterations, threshold_ransac, sift_matches{1}, keypoints{1}, model.coord3d, camera_params);
 
 % Take the indexes of the inliers
 inliers_2Dimage = sift_matches{1}(1,best_inliers_set);
@@ -139,17 +150,16 @@ cam_in_world_locations(:,:, 1) = init_location;
 % Visualise the pose for the initial frame
 edges = [[1, 1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 7]
     [2, 4, 5, 3, 6, 4, 7, 8, 6, 8, 7, 8]];
-figure()
-hold on;
+figure
 imshow(char(Filenames(1)), 'InitialMagnification', 'fit');
 title(sprintf('Initial Image Camera Pose'));
+hold on
 %   Plot bounding box
 points = project3d2image(vertices',camera_params, cam_in_world_orientations(:,:,1), cam_in_world_locations(:, :, 1));
 for j=1:12
     plot(points(1, edges(:, j)), points(2, edges(:,j)), 'color', 'b');
 end
 hold off;
-
 
 %% IRLS nonlinear optimisation
 
@@ -178,6 +188,155 @@ hold off;
 % either using Symbolic toolbox or finite differences approach
 
 % TODO: Implement IRLS method for the reprojection error optimisation
+
+%%%%%%%%% 1st Attempt Implementation %%%%%
+
+% Number of files -1 since we are going to use their scheme:
+% i is the initial frame (frame number 1) for which we have already
+% calculated the pose.
+% i + 1 is the subsequente frame (frame number 2) for which we want to
+% estimate the pose.
+
+
+for i=1 %:(num_files-1)
+    
+    %%%%%%%%%%%%%
+    %1) Project all SIFT keypoints from previous image,that have been matched, to 3D object. Use ray
+    %intersection.
+    
+    backProjected_point.coord3d = [];
+    backProjected_point.descriptors = [];
+    keypoint_3Dmodel = [];
+    %indx_keypoints = [];
+    
+    P = camera_params.IntrinsicMatrix.'*[cam_in_world_orientations(:,:,i) -cam_in_world_orientations(:,:,i)*cam_in_world_locations(:,:,i).'];
+    Q = P(:,1:3);
+    q = P(:,4);
+    orig = -inv(Q)*q; % this corresponds to C
+    
+    % We need to back project all sift points found on image i (first image
+    % in this case) to find which one intersect with the 3D model 
+    for h= 1:size(keypoints{i},2)
+        
+        % Create homogeneous coordinate point for one sift point
+        point = [keypoints{i}(1, h) keypoints{i}(2, h) 1];
+        % calculate ray from camera center through random selected keypoint
+        lambda = norm(inv(Q)*point.');
+        ray_m = orig + lambda * inv(Q)*point.';
+        
+        % iterate through faces
+        for k=1:size(faces)
+            % Skip iteration for not visible faces
+            %if(any(ismember(nan_index, faces(k, :))))
+            %    continue;
+            %end
+            
+            % calculate intersection ray/face (same as in hw1.1_task1)
+            [intersect, t, u, v, xcoor] = TriangleRayIntersection(orig', (ray_m - orig)',...
+                vertices(faces(k, 1), :), vertices(faces(k, 2), :), vertices(faces(k, 3), :), ...
+                'planeType','one sided');
+            
+            % save 3d intersection coordinates and descriptors of keypoint
+            if intersect ~= 0
+                % keypoints that have intersected the 3D model
+                keypoint_3Dmodel = [keypoint_3Dmodel, keypoints{i}(:,h)];
+                % index of those keypoints with respect to the keypoints
+                % matrix
+                % indx_keypoints = [indx_keypoints; h];
+                backProjected_point.coord3d = [backProjected_point.coord3d; xcoor];
+                backProjected_point.descriptors = [backProjected_point.descriptors descriptors{i}(:, h)];
+                %disp(xcoor);
+            end
+        end
+        
+    end
+    
+    % Images to check if the points that are said to belong to the model
+    % are inside the box in each image
+    figure(1)
+    imshow(char(Filenames(i)), 'InitialMagnification', 'fit')
+    title('SIFT points that have intersected with the 3D model when backprojected')
+    hold on;
+    scatter(keypoint_3Dmodel(1,:),keypoint_3Dmodel(2,:),'bo')
+    hold off;
+   
+    %%%%%%%%%%%%%
+    % 2) Find matches between SIFT keypoints from the initial frame (image 1) 
+    % that have intersected with the 3D model and the keypoints of the subsequent frame (image 2)
+    
+    threshold_ubcmatch = 2.5;
+    descriptors_backProjected_points = backProjected_point.descriptors;
+    
+    % Calculate the matches
+    [sift_matches{i+1} , sift_scores{i+1}]= vl_ubcmatch(descriptors_backProjected_points, descriptors{i+1}, threshold_ubcmatch);
+    
+    % sift_matches{2}(1,:) --> sift matches from the previous image, out of the ones that were
+    % backprojected to the model
+    % sift_matches{2}(2,:) ---> sift matches in the subsequent frame
+    
+    figure(2)
+    % Plot the image and the matched points on top.
+    imshow (char(Filenames(i+1)));
+    title('Matched SIFT points from current frame (i+1) on top of the current image')
+    hold on;
+    % Visualize the matched points on the frame we are currently trying to
+    % estimate the pose i+1
+    vl_plotframe(keypoints{i+1}(:,sift_matches{i+1}(2,:)), 'linewidth',2)
+    %plot (keypoints{h}(1, sift_matches{h}(1,:)), keypoints{h}(2, sift_matches{h}(1,:)), 'r*');
+    hold off;
+    
+    %%%%%%%%%%%%%
+    % 3) Reproject these matches using corresponding 3D coordinates from the
+    % step 1 and the initial camera pose back to the subsequent frame (image i+1)
+    
+    % Get the 3D coordinates of the backprojected points coming from the
+    % initial frame
+    backProjectedPoints_3Dcoord = backProjected_point.coord3d(sift_matches{i+1}(1,:),:);
+    
+    % Project match points from 3D model to the current frame (i+1) (2D image) 
+    reprojected_points = project3d2image(backProjectedPoints_3Dcoord',camera_params, cam_in_world_orientations(:,:,i), cam_in_world_locations(:, :, i));
+       
+    %%%%%%%%%%%%%
+    % 4) Compute the reprojection error between pixels from SIFT
+    % matches for the subsequent frame (image i+1) and from reprojected matches
+    % from step 3
+    image_points = [keypoints{i+1}(1,sift_matches{i+1}(2,:));keypoints{i+1}(2,sift_matches{i+1}(2,:))];
+    
+    % Calculate the error of reprojected the points
+    distance_reprojection = pdist2(reprojected_points',image_points');
+    
+    % We take the diagonal because pdist2 gets the distance between all
+    % points to each other, and we only need each point to eachself.
+    d = diag(distance_reprojection);
+    
+    % Figure to see the comparison between the points that were matched
+    % between the SIFT points of the previous and current frame, vs the
+    % reprojected matched points from the 3D model.
+    figure(3)
+    imshow(char(Filenames(i+1)))
+    title('Reprojected points from 3D model vs Matched points')
+    hold on;
+    scatter(reprojected_points(1,:)',reprojected_points(2,:)','rx')
+    scatter(image_points(1,:)',image_points(2,:)','bo')
+    legend('Reprojected points','Matched points from this image')
+    hold off;
+    
+    %%%%%%%%%%%%%
+    % 5) Compute Jacobian of the reprojection error with respect to the pose
+    % parameters and apply IRLS to iteratively update the camera pose for the subsequent frame (image i+1)
+    
+    
+    %%%%%%%%%%%%%
+    % 6) Now the subsequent frame (image i+1) becomes the initial frame for the
+    % next subsequent frame (image i+2) and the method continues until camera poses for all
+    % images are estimated
+    
+    
+    
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 
 %% Plot camera trajectory in 3D world CS + cameras
