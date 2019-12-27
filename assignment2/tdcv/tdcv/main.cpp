38f6a0c463b58aef24126296c53e09b206dcc398
ge25hex <ge25hex@mytum.de>
2019-12-27 16:28:10 +0100

    Task 1 completed

--- assignment2/tdcv/tdcv/main.cpp
+++ assignment2/tdcv/tdcv/main.cpp
@@ -5,44 +5,158 @@
 //  Created by Fernando Benito Abad on 22.12.19.
 //  Copyright © 2019 Fernando Benito Abad. All rights reserved.
 //
-
 #include <opencv2/opencv.hpp>
 #include <iostream>
 using namespace std;
 using namespace cv;
 
-Mat rotate(Mat src, double angle)
-{
-    Mat dst;
-    Point2f pt(src.cols/2., src.rows/2.);
-    Mat r = getRotationMatrix2D(pt, angle, 1.0);
-    warpAffine(src, dst, r, Size(src.cols, src.rows));
-    return dst;
+void visualizeHOG(Mat img, std::vector<float> feats, HOGDescriptor hog_detector, int scale_factor) {
+
+    Mat visual_image;
+    resize(img, visual_image, Size(img.cols * scale_factor, img.rows * scale_factor));
+
+    int n_bins = hog_detector.nbins;
+    float rad_per_bin = 3.14 / (float) n_bins;
+    Size win_size = hog_detector.winSize;
+    Size cell_size = hog_detector.cellSize;
+    Size block_size = hog_detector.blockSize;
+    Size block_stride = hog_detector.blockStride;
+
+    // prepare data structure: 9 orientation / gradient strenghts for each cell
+    int cells_in_x_dir = win_size.width / cell_size.width;
+    int cells_in_y_dir = win_size.height / cell_size.height;
+    int n_cells = cells_in_x_dir * cells_in_y_dir;
+    int cells_per_block = (block_size.width / cell_size.width) * (block_size.height / cell_size.height);
+
+    int blocks_in_x_dir = (win_size.width - block_size.width) / block_stride.width + 1;
+    int blocks_in_y_dir = (win_size.height - block_size.height) / block_stride.height + 1;
+    int n_blocks = blocks_in_x_dir * blocks_in_y_dir;
+
+    float ***gradientStrengths = new float **[cells_in_y_dir];
+    int **cellUpdateCounter = new int *[cells_in_y_dir];
+    for (int y = 0; y < cells_in_y_dir; y++) {
+        gradientStrengths[y] = new float *[cells_in_x_dir];
+        cellUpdateCounter[y] = new int[cells_in_x_dir];
+        for (int x = 0; x < cells_in_x_dir; x++) {
+            gradientStrengths[y][x] = new float[n_bins];
+            cellUpdateCounter[y][x] = 0;
+
+            for (int bin = 0; bin < n_bins; bin++)
+                gradientStrengths[y][x][bin] = 0.0;
+        }
+    }
+
+    // compute gradient strengths per cell
+    int descriptorDataIdx = 0;
+
+    for (int block_x = 0; block_x < blocks_in_x_dir; block_x++) {
+        for (int block_y = 0; block_y < blocks_in_y_dir; block_y++) {
+            int cell_start_x = block_x * block_stride.width / cell_size.width;
+            int cell_start_y = block_y * block_stride.height / cell_size.height;
+
+            for (int cell_id_x = cell_start_x;
+                 cell_id_x < cell_start_x + block_size.width / cell_size.width; cell_id_x++)
+                for (int cell_id_y = cell_start_y;
+                     cell_id_y < cell_start_y + block_size.height / cell_size.height; cell_id_y++) {
+
+                    for (int bin = 0; bin < n_bins; bin++) {
+                        float val = feats.at(descriptorDataIdx++);
+                        gradientStrengths[cell_id_y][cell_id_x][bin] += val;
+                    }
+                    cellUpdateCounter[cell_id_y][cell_id_x]++;
+                }
+        }
+    }
+    
+    // compute average gradient strengths
+    for (int celly = 0; celly < cells_in_y_dir; celly++) {
+        for (int cellx = 0; cellx < cells_in_x_dir; cellx++) {
+
+            float NrUpdatesForThisCell = (float) cellUpdateCounter[celly][cellx];
+
+            // compute average gradient strenghts for each gradient bin direction
+            for (int bin = 0; bin < n_bins; bin++) {
+                gradientStrengths[celly][cellx][bin] /= NrUpdatesForThisCell;
+            }
+        }
+    }
+
+    for (int celly = 0; celly < cells_in_y_dir; celly++) {
+        for (int cellx = 0; cellx < cells_in_x_dir; cellx++) {
+            int drawX = cellx * cell_size.width;
+            int drawY = celly * cell_size.height;
+
+            int mx = drawX + cell_size.width / 2;
+            int my = drawY + cell_size.height / 2;
+
+            rectangle(visual_image,
+                      Point(drawX * scale_factor, drawY * scale_factor),
+                      Point((drawX + cell_size.width) * scale_factor,
+                                (drawY + cell_size.height) * scale_factor),
+                      CV_RGB(100, 100, 100),
+                      1);
+
+            for (int bin = 0; bin < n_bins; bin++) {
+                float currentGradStrength = gradientStrengths[celly][cellx][bin];
+
+                if (currentGradStrength == 0)
+                    continue;
+
+                float currRad = bin * rad_per_bin + rad_per_bin / 2;
+
+                float dirVecX = cos(currRad);
+                float dirVecY = sin(currRad);
+                float maxVecLen = cell_size.width / 2;
+                float scale = scale_factor / 5.0; // just a visual_imagealization scale,
+
+                // compute line coordinates
+                float x1 = mx - dirVecX * currentGradStrength * maxVecLen * scale;
+                float y1 = my - dirVecY * currentGradStrength * maxVecLen * scale;
+                float x2 = mx + dirVecX * currentGradStrength * maxVecLen * scale;
+                float y2 = my + dirVecY * currentGradStrength * maxVecLen * scale;
+
+                // draw gradient visual_imagealization
+                line(visual_image,
+                     Point(x1 * scale_factor, y1 * scale_factor),
+                     Point(x2 * scale_factor, y2 * scale_factor),
+                     CV_RGB(0, 0, 255),
+                     1);
+            }
+
+        }
+    }
+    
+    for (int y = 0; y < cells_in_y_dir; y++) {
+        for (int x = 0; x < cells_in_x_dir; x++) {
+            delete[] gradientStrengths[y][x];
+        }
+        delete[] gradientStrengths[y];
+        delete[] cellUpdateCounter[y];
+    }
+    delete[] gradientStrengths;
+    delete[] cellUpdateCounter;
+    imshow("HOG vis", visual_image);
+    waitKey(-1);
+    imwrite("hog_vis.jpg", visual_image);
+
 }
+
+
 int main(int argc, const char * argv[]) {
     Mat img = imread("data/task1/obj1000.jpg");
-    img.convertTo(img, CV_32F, 1/255.0);
-    Mat gx, gy, mag, angle;
-    Sobel(img, gx, CV_32F, 1, 0, 1);
-    Sobel(img, gy, CV_32F, 0, 1, 1);
-    cartToPolar(gx, gy, mag, angle, 1);
-    Mat img_temp;
-    // create descriptor object and set the size of the hog descriptor to image size
-    /*cv::HOGDescriptor hog;
-    hog.winSize = grayImg.size();*/
-    for (int i=0; i<360/10; i++) {
-        img_temp = rotate(mag, i*10);
-        namedWindow("image", WINDOW_NORMAL);
-        imshow("image", img_temp);
-        waitKey(100);
-    }
-    // set the descriptor position to the middle of the image
-    /*std::vector<cv::Point> positions;
-    positions.push_back(cv::Point(grayImg.cols / 2, grayImg.rows / 2));
-    std::vector<float> descriptor;
-    hog.compute(grayImg,descriptor,cv::Size(),cv::Size(),positions);
-    namedWindow("image", WINDOW_NORMAL);
-    imshow("image", grayImg);
-    waitKey(0);*/
+    Mat grayImg;
+
+    resize(img, img, Size(64, 64));
+    cvtColor(img, grayImg, COLOR_RGB2GRAY);
+    
+    HOGDescriptor myHog(Size(64, 64), Size(8, 8), Size(4, 4), Size(4, 4), 9);
+    vector< float> feat_values;
+    vector< Point> locations;
+    myHog.compute(grayImg, feat_values, Size(0, 0), Size(0, 0), locations);
+    visualizeHOG(img, feat_values, HOGDescriptor (Size(64, 64), Size(8, 8), Size(4, 4), Size(4, 4), 9), 5);
+    /*for ( const auto &row : feat_vect ){
+        visualizeHOG(img, row, myHog, 1);
+    }*/
+    
     return 0;
 }
