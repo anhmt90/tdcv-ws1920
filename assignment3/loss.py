cd5b78c5337dbc5e508821dc99af7a8835acd411
Tuan Anh <anh.ma@tum.de>
2020-02-02 04:57:21 +0100

    fix bug triple loss has the norms swapped wrt the devision

--- assignment3/loss.py
+++ assignment3/loss.py
@@ -7,7 +7,7 @@ def triplet_loss(output):
     diff_neg = output[0:batch_size:3] - output[2:batch_size:3]
     norm_pos = torch.norm(diff_pos.view(batch_size//3, -1), p=2, dim=1)
     norm_neg = torch.norm(diff_neg.view(batch_size//3, -1), p=2, dim=1)
-    loss = torch.max(torch.zeros(batch_size//3).to(output.device), 1 - (norm_pos**2 / (norm_neg**2 + 0.01)))
+    loss = torch.max(torch.zeros(batch_size//3).to(output.device), 1 - (norm_neg**2 / (norm_pos**2 + 0.01)))
     return loss.sum()
 
 
